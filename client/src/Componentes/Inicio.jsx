02a69fdc8acf90a33c870e1735a603c81e7ab9b6
Gonzalo Lucero <glucero@adecoagro.com>
2022-06-15 15:33:12 -0300

    cambios ok

--- client/src/Componentes/Inicio.jsx
+++ client/src/Componentes/Inicio.jsx
@@ -1,23 +1,66 @@
 import { getCryptos } from "../actions/index";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "../EstilosCss/Inicio.css"
-import back from "./images/wallpaper.jpg"
+// import "../EstilosCss/Inicio.css"
+import back from "./images/hola.jpg"
+import { useDispatch } from "react-redux";
+export default function Inicio() {
 
-export default function LandingPage() {
+  const dispatch = useDispatch();
+  const [nombre, setNombre] = useState(""); // declaro un estado inicial nombre, inicialmente vacio
+  // en nombre guardo, lo que escribo en la busqueda
+  function handleSubmit(e) { // cuando se accione, reseteo el estado 
+      e.preventDefault();
+      setNombre(""); 
+  }
+  
+  function buscar (){
+      if(nombre != ''){
+         
+          // dispatch(search(nombre));
+          
+          console.log('llego aqiii')
+      }
+      
+  }
  
 
   return (
-      <div  style={{backgroundColor:'black' ,height:'0px', borderColor:'black'}}>
-      <img class="backk" src={back} alt="" />
+    <div className="div1">
+      <div className="div">
+        <h1>LA CALLE ANDA DICIENDO</h1>
+        <input type="input" className="input" placeholder="Frase..." />
+        <button type="submit" className="button" onClick={buscar}> Login </button>
+      </div>
+      </div>
 
-  <div style={{backgroundColor:'black', height:'0px'}}>
-        <Link to="/home">
-          <button className='button' type="submit">ENTER</button>
-        </Link>
-        </div>
-
-        </div>
 
 
   );
 }
+
+
+  //     <div className="main">
+  //     <img class="backk" src={back} alt="" />
+  //     <div className="searchbarr">
+  //               <form onSubmit={(e) => handleSubmit(e)}> 
+  //                   <input
+  //                   value={nombre}
+  //                   onChange={(e) => setNombre(e.target.value)}
+  //                   placeholder="Search..."
+                    
+  //                   type="input"
+  //                   ></input>
+  //                    <Link to="/home" onClick={() => buscar()}>
+  //                       <button name="name" type="submit" >🔍ENTER</button>
+  //                       </Link>
+  //               </form>
+  //           </div>
+
+  // <div>
+  //       <Link to="/home">
+  //         <button className='button' type="submit">ENTER</button>
+  //       </Link>
+  //       </div>
+
+  //       </div>
